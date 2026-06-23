57b9c22afa24b2e3cb87cc62bb0a742a846ddf07
djgva <djgva@BIG-DILL-ROC>
2026-06-23 14:58:06 +0200

    Added the class Playlsit, in the class there is a feature called CreatePlaylist. This make a playlist where the user can add a custom name

--- Playlist.cs
+++ Playlist.cs
@@ -5,15 +5,19 @@ namespace Predify
 {
     public class Playlist
     {
+        // Naam van de playlist
         public string Name { get; set; }
+        // Lijst van nummers in de playlist
         public List<string> Songs { get; set; }
 
+        // Constructor om een playlist aan te maken met een naam
         public Playlist(string name)
         {
             Name = name;
             Songs = new List<string>();
         }
 
+        // Vraag de gebruiker om een naam en maak de playlist aan
         public void CreatePlaylist()
         {
             Console.Write("Geef de naam van de playlist: ");
