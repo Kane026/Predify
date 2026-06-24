9d3ca50bac5ccecc2b731f549795338490eb9f5d
djgva <djgva@BIG-DILL-ROC>
2026-06-24 10:33:32 +0200

    Added the function that you can add songs to the playlist, already prepared Program for the next user story

--- Playlist.cs
+++ Playlist.cs
@@ -24,8 +24,30 @@ namespace Predify
             string name = Console.ReadLine();
             Name = name;
             Console.WriteLine("Playlist '" + Name + "' aangemaakt.");
-            Console.WriteLine("Druk op Enter om terug te gaan.");
-            Console.ReadLine();
+        }
+
+        // Toon een menu om een nummer te kiezen en toe te voegen
+        public void AddSong()
+        {
+            // Beschikbare nummers
+            List<string> availableSongs = new List<string>
+            {
+                "Never See Me Again - Kanye West - Hip Hop",
+                "Mama's Boyfriend - Kanye West - Hip Hop",
+                "Flashing Light - Kanye West - Hip Hop"
+            };
+
+            // Toon de beschikbare nummers
+            Console.WriteLine("Kies een nummer om toe te voegen:");
+            for (int i = 0; i < availableSongs.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + availableSongs[i]);
+            }
+
+            // Voeg het gekozen nummer toe aan de playlist
+            int choice = int.Parse(Console.ReadLine());
+            Songs.Add(availableSongs[choice - 1]);
+            Console.WriteLine("Nummer toegevoegd aan playlist '" + Name + "'.");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,45 +4,49 @@ class Program
 {
     static void Main()
     {
-        // Display the main menu
-        Console.WriteLine("1. Muziek afspelen");
-        Console.WriteLine("2. Nummers");
-        Console.WriteLine("3. Playlist");
-        Console.WriteLine("4. Albums");
-        Console.WriteLine("5. Afsluiten");
+        while (true)
+        {
+            // Display the main menu
+            Console.WriteLine("1. Nummers");
+            Console.WriteLine("2. Playlist aanmaken");
+            Console.WriteLine("3. Albums");
+            Console.WriteLine("4. Playlist inkijken");
+            Console.WriteLine("5. Afsluiten");
 
-        // Ask the user to make a choice
-        Console.Write("Maak een keuze: ");
-        string choice = Console.ReadLine();
+            // Ask the user to make a choice
+            Console.Write("Maak een keuze: ");
+            string choice = Console.ReadLine();
 
-        // Handle the user's choice
-        if (choice == "1")
-        {
-            Console.WriteLine("Muziek afspelen gekozen");
-        }
-        else if (choice == "2")
-        {
-            // Show the song list
-            var songList = new SongList();
-            songList.ShowSongs();
-        }
-        else if (choice == "3")
-        {
-            Playlist playlist = new Playlist("");
-            playlist.CreatePlaylist();
-        }
-        else if (choice == "4")
-        {
-            Console.WriteLine("Albums gekozen");
-        }
-        else if (choice == "5")
-        {
-            Console.WriteLine("Afsluiten gekozen");
-        }
-        else
-        {
-            // Invalid choice
-            Console.WriteLine("Geen geldige keuze");
+            // Handle the user's choice
+            if (choice == "1")
+            {
+                var songList = new SongList();
+                songList.ShowSongs();
+            }
+            else if (choice == "2")
+            {
+                Playlist playlist = new Playlist("");
+                playlist.CreatePlaylist();
+                playlist.AddSongMenu();
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("Albums gekozen");
+            }
+            else if (choice == "4")
+            {
+                
+                Console.WriteLine("Playlist inkijken");
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Afsluiten gekozen");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Geen geldige keuze");
+            }
         }
     }
 }
